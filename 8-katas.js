dbd67f2b92214ec35367cc4886c841af349a9233
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-03 02:25:48 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -654,3 +654,11 @@ var max = function(list){
 /*
   Count the Monkeys!
 */
+
+function monkeyCount(n) {
+  let array = []
+  for (let i = 1; i <= n; i++) {
+    array.push(i)
+  }
+  return array
+}
