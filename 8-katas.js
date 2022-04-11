9a844c9e0a27114b6788b35d58b7a0b18d5a2d7e
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-10 20:46:17 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -832,3 +832,22 @@ function countBy(x, n) {
   
   return z;
 }
+
+
+
+/*
+  Find the first non-consecutive number
+*/
+
+function firstNonConsecutive (arr) {
+  if (arr.length < 2)
+    return null
+  
+  for (let i = 1; i < arr.length; i++) {
+    let diff = arr[i] - arr[i - 1]
+    if (diff !== 1)
+      return arr[i]
+  }
+  
+  return null
+}
