d8c171d4e34110baa8deaf7e8304faef2d9b6a7e
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-14 21:28:35 -0700

    add date folder for organization

--- 8-katas.js
+++ 8-katas.js
@@ -1016,3 +1016,56 @@ function doubleChar(str) {
 function setAlarm(employed, vacation){
   return employed && !vacation
 }
+
+
+
+/*
+  Get Planet Name By ID
+*/
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break
+    case 2:
+      name = 'Venus'
+      break
+    case 3:
+      name = 'Earth'
+      break
+    case 4:
+      name = 'Mars'
+      break
+    case 5:
+      name = 'Jupiter'
+      break
+    case 6:
+      name = 'Saturn'
+      break
+    case 7:
+      name = 'Uranus'
+      break
+    case 8:
+      name = 'Neptune'
+      break
+  }
+  
+  return name;
+}
+
+// Missing breaks and technically missing a default also adding a return could negate the need of the break
+// A clever solution uses an object.
+// function getPlanetName(id){
+//   return {
+//     1: 'Mercury',
+//     2: 'Venus',
+//     3: 'Earth',
+//     4: 'Mars',
+//     5: 'Jupiter',
+//     6: 'Saturn',
+//     7: 'Uranus',
+//     8: 'Neptune'
+//   }[id]
+// }
