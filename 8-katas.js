9d13af1d79b54addfcccb09b0176e0d701f4fd4e
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-16 14:10:37 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -1237,3 +1237,34 @@ function strCount(str, letter){
 }
 
 // A similar solution would be to split on the desired letter and return length - 1
+
+
+
+/*
+  Switch it Up!
+*/
+
+function switchItUp(number){
+  switch (number) {
+    case 1:
+      return 'One'
+    case 2:
+      return 'Two'
+    case 3:
+      return 'Three'
+    case 4:
+      return 'Four'
+    case 5:
+      return 'Five'
+    case 6:
+      return 'Six'
+    case 7:
+      return 'Seven'
+    case 8:
+      return 'Eight'
+    case 9:
+      return 'Nine'
+    case 0:
+      return 'Zero'
+  }
+}
