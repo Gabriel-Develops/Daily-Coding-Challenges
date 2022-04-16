fb5f4d00b81481a40c8050f4eae6d89c8e4653d6
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-16 14:16:01 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -1268,3 +1268,21 @@ function switchItUp(number){
       return 'Zero'
   }
 }
+
+
+
+/*
+  Squash the bugs
+*/
+
+function findLongest(str) {
+  
+  let spl = str.split(" ")
+  let longest = 0
+  
+  for (let i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest)
+      longest = spl[i].length
+  }
+  return longest
+}
