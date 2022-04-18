55a7905d8c553955b746ff4c34ee270bcb1b9a7d
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-17 17:44:11 -0700

    Add kata via upload

--- 7-katas.js
+++ 7-katas.js
@@ -10,3 +10,18 @@ function getCount(str) {
   // Return length of filtered array
   return str.split('').filter(c => vowels.includes(c)).length
 }
+
+
+
+/*
+  Square Every Digit
+*/
+
+function squareDigits(num){
+  return +num.toString().split('').map(element => Math.pow(element, 2)).join('')
+}
+
+// First convert to string, split into array of single digits
+// For every digit, double it using Math.pow
+// Join all elements in array
+// Lastly using the unary (+) operator, convert result to a number
