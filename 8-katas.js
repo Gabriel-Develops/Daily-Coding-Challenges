7f554f56f820cb3f7e4d1675d6851a16870d20f9
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-18 22:59:44 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -1544,3 +1544,18 @@ function testEven(n) {
 function move (position, roll) {
   return position + (roll * 2)
 }
+
+
+
+/*
+  What's the real floor?
+*/
+
+function getRealFloor(n) {
+  if (n <= 0)
+    return n
+  else if (n < 13)
+    return n - 1
+  else
+    return n - 2
+}
