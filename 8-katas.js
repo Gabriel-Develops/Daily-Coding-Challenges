9e7b642ce3aff87d49fc4342eda49978cf1d5a76
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-19 19:01:36 -0700

    add html files for past-only project website, unlinked

--- 8-katas.js
+++ 8-katas.js
@@ -1595,3 +1595,20 @@ function warnTheSheep(queue) {
 function problem(x){
   return typeof x === "number" ? x * 50 + 6 : "Error";
 }
+
+
+
+/*
+  Training JS #7: if..else and ternary operator
+*/
+
+function saleHotdogs(n){
+  return n < 5 ? n * 100 : n < 10 ? n * 95 : n * 90
+}
+
+// if (n < 5)
+//   return n * 100
+// else if (n < 10)
+//   return n * 95
+// else
+//   return n * 90
