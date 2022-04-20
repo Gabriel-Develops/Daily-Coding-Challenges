96b5a5ea6986ba1af01b37689d02ec4521250169
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-20 12:23:40 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -1669,3 +1669,19 @@ function unusualFive() {
 */
 
 const square = num => num ** 2
+
+
+
+/*
+  Filling an array (part 1)
+*/
+
+const arr = N => {
+  let arr = []
+  
+  for (let i = 0; i < N; i++) {
+    arr.push(i)
+  }
+  
+  return arr
+}
