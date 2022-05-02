25ee0e3ffe82b5c034ebdc612fe7a40ce22b32d2
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-05-01 17:05:02 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -2199,3 +2199,33 @@ function position(letter){
 
 // return letter.charCodeAt() - 96
 // return letter.toLowerCase().charCodeAt() - 96
+
+
+
+/*
+  Training JS #4: Basic data types--Array
+*/
+
+function getLength(arr){
+  //return length of arr
+  return arr.length
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length - 1]
+}
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+  arr.push(el)
+  return arr
+}
+function popElement(arr){
+  //pop an element from arr
+  arr.pop()
+  return arr
+}
