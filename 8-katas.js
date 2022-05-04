f65dd8f7f094df7f7d1727db20179a09c96ed23b
Gabriel Rodriguez <garodri97@gmail.com>
2022-05-04 14:31:31 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -2379,3 +2379,17 @@ function main (verb, noun) {
 function main (verb, noun) {
   return verb + noun
 }
+
+
+
+/*
+  Fix your code before the garden dies! 
+*/
+
+function rainAmount(mm) {
+  if (mm < 40) {
+    return `You need to give your plant ${40 - mm}mm of water`
+  } else {
+    return 'Your plant has had more than enough water for today!'
+  }
+}
