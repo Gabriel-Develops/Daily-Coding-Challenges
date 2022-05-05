0ce8bc812b46f7a8152592950001f98ad4b3fe2c
Gabriel Rodriguez <garodri97@gmail.com>
2022-05-04 20:12:47 -0700

    Add kata via upload

--- 6-katas.js
+++ 6-katas.js
@@ -1,4 +1,23 @@
 /*
-  
+  Highest Scoring Word
 */
 
+function high(x){
+  let strArray = x.split(' ')
+  let highest = {score: 0, word: ''}
+  
+  for (let word of strArray) {
+    let score = 0
+    
+    for (let c of word) {
+      score += c.toLowerCase().charCodeAt() - 96
+    }
+    
+    if (score > highest.score) {
+      highest.score = score
+      highest.word = word
+    }
+  }
+  
+  return highest.word
+}
