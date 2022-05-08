27da1480676ead533ad19d2ee3435aab3484e89a
Gabriel Rodriguez <garodri97@gmail.com>
2022-05-07 17:33:32 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -2518,3 +2518,13 @@ function remove(s, n) {
   
   return s
 }
+
+
+
+/*
+  Training JS #5: Basic data types--Object
+*/
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
