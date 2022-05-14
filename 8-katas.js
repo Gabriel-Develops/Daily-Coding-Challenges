4604c4fc5c2941328972544336bb9ef36195394d
Gabriel Rodriguez <garodri97@gmail.com>
2022-05-13 17:45:32 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -2975,3 +2975,40 @@ function pickIt(arr) {
   
 //   return [odd,even];
 // }
+
+
+
+/*
+  Training JS #8: Conditional statement--switch
+*/
+
+function howManydays(month) {
+  let days;
+  switch (month) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+          days = 31;
+          break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+          days = 30;
+          break;
+      case 2:
+          days = 28;
+          break;
+  }
+  return days;
+}
+
+// You can add cases to one line
+// case 1: case 2: case 3:
+//    do something
+//    break
+// Another tip would be to use the default value to represent the months that have 31 days to reduce the size of the switch.
