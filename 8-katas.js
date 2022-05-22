7d3e1d031c6bf75906da4aa70ad3140fabfe838e
Gabriel Rodriguez <garodri97@gmail.com>
2022-05-22 00:38:48 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -3406,3 +3406,20 @@ function getNumberFromString(s) {
 */
 
 const getASCII = c => c.charCodeAt();
+
+
+
+/*
+  Leonardo Dicaprio and Oscars
+*/
+
+function leo(oscar){
+  if (oscar === 88)
+    return 'Leo finally won the oscar! Leo is happy'
+  else if (oscar === 86)
+    return 'Not even for Wolf of wallstreet?!'
+  else if (oscar > 88)
+    return 'Leo got one already!'
+  else
+    return 'When will you give Leo an Oscar?'
+}
