218a2c69bf550953690b581811eafd279fef09e4
Gabriel Rodriguez <garodri97@gmail.com>
2022-05-23 15:22:04 -0700

    Add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -3476,3 +3476,17 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
   
   return blueLeft / totalMarbles
 }
+
+
+
+/*
+  Fix the Bugs (Syntax) - My First Kata
+*/
+
+function myFirstKata(a, b) {
+  if (typeof(a) !== "number" || typeof(b) !== "number") {
+    return false;
+  } else {
+    return a % b + b % a;
+  }
+}
