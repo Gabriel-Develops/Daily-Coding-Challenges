1ad309ee89385a8796ffe4b2d9b0ca55530da06d
Gabriel Rodriguez <garodri97@gmail.com>
2022-06-30 15:49:11 -0700

    add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -4319,3 +4319,13 @@ function firstToLast(str, c) {
   return str.includes(c) ? str.lastIndexOf(c) - str.indexOf(c) : -1;
   
 }
+
+
+
+/*
+  Is your period late?
+*/
+
+function periodIsLate(last, today, cycleLength){
+  return (today-last)/(1000*60*60*24) > cycleLength;
+}
