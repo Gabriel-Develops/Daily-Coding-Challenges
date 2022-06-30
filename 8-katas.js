eb65db2e2e0a187bc7a9eac2eefae7b080f6d1d5
Gabriel Rodriguez <garodri97@gmail.com>
2022-06-30 02:24:04 -0700

    add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -4308,3 +4308,14 @@ function howManySmaller(arr,n){
   }
   return count
 }
+
+
+
+/*
+  Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()
+*/
+
+function firstToLast(str, c) {
+  return str.includes(c) ? str.lastIndexOf(c) - str.indexOf(c) : -1;
+  
+}
