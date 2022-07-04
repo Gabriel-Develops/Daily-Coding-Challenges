69d46ada71887a708f7f0088a23e7b6e79508d62
Gabriel Rodriguez <garodri97@gmail.com>
2022-07-03 19:25:22 -0700

    add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -4360,3 +4360,11 @@ function yearDays(year)
     return `${year} has 365 days`;
   }
 }
+
+
+
+/*
+  Coefficients of the Quadratic Equation
+*/
+
+const quadratic = (x1, x2) => [1, -x1+(-x2), -x1*-x2]
