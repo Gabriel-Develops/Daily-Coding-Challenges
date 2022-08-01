c80e917d3b0e15b90894487d4e7b7ed2769a05a0
Gabriel Rodriguez <garodri97@gmail.com>
2022-07-31 23:04:13 -0700

    add kata via upload

--- 8-katas.js
+++ 8-katas.js
@@ -4644,3 +4644,28 @@ function roundIt(n){
   else if (left > right) return Math.floor(n)
   else return Math.round(n)
 }
+
+
+
+/*
+  Training JS #13: Number object and its properties
+*/
+
+function whatNumberIsIt(n){
+  if (Number.isNaN(n)) {
+    return "Input number is Number.NaN";
+  }
+  
+  switch (n) {
+  case Number.MAX_VALUE:
+    return "Input number is Number.MAX_VALUE";
+  case Number.MIN_VALUE:
+    return "Input number is Number.MIN_VALUE";
+  case Number.NEGATIVE_INFINITY:
+    return "Input number is Number.NEGATIVE_INFINITY";
+  case Number.POSITIVE_INFINITY:
+    return "Input number is Number.POSITIVE_INFINITY";
+  default:
+    return `Input number is ${n}`
+  }
+}
