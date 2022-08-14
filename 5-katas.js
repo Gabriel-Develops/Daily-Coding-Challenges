5a52af636edbb54d08dfc8ae85c9dcd6acce7750
Gabriel Rodriguez <garodri97@gmail.com>
2022-08-14 13:05:38 -0700

    add kata via upload

--- 5-katas.js
+++ 5-katas.js
@@ -20,3 +20,31 @@ function dirReduc(arr){
   
   return arr
 }
+
+
+
+/*
+  Rot13
+*/
+
+function rot13(message){
+  let cipher = []
+  
+  for (let i = 0; i < message.length; i++) {
+    // Uppercase
+    if (message[i] >= 'A' && message[i] <= 'Z')
+      cipher.push((message.charCodeAt(i) - 65 + 13) % 26 + 65)
+    
+    // Lowercase
+    else if (message[i] >= 'a' && message[i] <= 'z')
+      cipher.push((message.charCodeAt(i) - 97 + 13) % 26 + 97)
+    
+    // Not a letter
+    else
+      cipher.push(message.charCodeAt(i))
+    
+    
+  }
+  
+  return String.fromCharCode(...cipher)
+}
