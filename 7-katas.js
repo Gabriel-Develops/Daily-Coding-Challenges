02f87f43b992e4d1f43d0853a40f634d3eed6e3f
Gabriel Rodriguez <garodri97@gmail.com>
2022-09-03 23:36:18 -0700

    add kata via upload

--- 7-katas.js
+++ 7-katas.js
@@ -843,3 +843,19 @@ function bump(x){
          ? "Car Dead" 
          :"Woohoo!"
 }
+
+
+
+/*
+  Moves in squared strings (I)
+*/
+
+function vertMirror(strng) {
+  return strng.split('\n').map(x => x.split('').reverse().join('')).join('\n')
+}
+function horMirror(strng) {
+  return strng.split('\n').reverse().join(`\n`)
+}
+function oper(fct, s) {
+  return fct(s)
+}
