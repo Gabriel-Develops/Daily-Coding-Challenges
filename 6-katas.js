de58f378c618c6effe9cf99303db098c616f576e
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-18 13:48:56 -0700

    add kata via upload

--- 6-katas.js
+++ 6-katas.js
@@ -450,3 +450,17 @@ function wave(str){
     // Filter out empty strings 
     .filter(x => x)
 }
+
+
+
+/*
+  Reverse every other word in the string
+*/
+
+function reverse(str){
+  return str.split(' ')
+    .map((x, i) => i % 2 === 0 ? x : x.split('').reverse().join(''))
+    .filter(x => x)
+    .join(' ')
+    .trim()
+}
