31bdaf81696e0ad31fb538bc089c14b2082f4ad6
Gabriel Rodriguez <garodri97@gmail.com>
2022-11-14 10:53:02 -0800

    add kata via upload

--- 7-katas.js
+++ 7-katas.js
@@ -1831,3 +1831,17 @@ function averages(numbers) {
   }
   return avgArr
 }
+
+
+
+/*
+  esreveR
+*/
+
+const reverse = array => {
+  const reversedArr = []
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArr.push(array[i])
+  }
+  return reversedArr
+}
