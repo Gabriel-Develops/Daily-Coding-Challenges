e7191c1f6249a37df3b346f39a3444c50d8f94bf
Gabriel Rodriguez <garodri97@gmail.com>
2022-12-06 12:33:52 -0800

    started leetcode file and upload of first two leetcodes

---
+++ leetcode.js
@@ -0,0 +1,24 @@
+// 26. Remove Duplicates from Sorted Array
+
+const removeDuplicates = nums => {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1)
+            i -= 1
+        }
+    }
+    return nums.length
+}
+
+
+
+771. Jewels and Stones
+
+const numJewelsInStones = (jewels, stones) => {
+    let sum = 0
+    for (let stone of stones) {
+        if (jewels.includes(stone))
+            sum++
+    }
+    return sum
+};
