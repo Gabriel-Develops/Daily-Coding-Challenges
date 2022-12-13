cd3c2d47b038bc5a8577fcc8b0ba99b4b4ddb9a8
Gabriel Rodriguez <garodri97@gmail.com>
2022-12-13 13:26:43 -0800

    add leetcode via upload

--- leetcode.js
+++ leetcode.js
@@ -30,3 +30,28 @@ const numJewelsInStones = (jewels, stones) => {
 var containsDuplicate = function(nums) {
     return nums.some(x => nums.indexOf(x) !== nums.lastIndexOf(x))
 };
+
+
+
+// 169. Majority Element
+
+var majorityElement = function(nums) {
+    let numMap = new Map()
+    for (let num of nums) {
+        numMap.set(num, numMap.get(num) + 1 || 1)
+    }
+    for (let [num, count] of numMap) {
+        if (count > nums.length / 2)
+            return num
+    }
+};
+
+// We can use a reduce method as we simplifying the array into a single element, a Map
+
+var majorityElement = function(nums) {
+    const numMap = nums.reduce((numMap, num) => numMap.set(num, numMap.get(num) + 1 || 1), new Map())
+    for (const [num, count] of numMap) {
+        if (count > nums.length / 2)
+            return num
+    }
+};
